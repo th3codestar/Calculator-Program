b0f8f84e25fc60421ec745b27f2bd6d5f6713a65
th3codestar <155606660+th3codestar@users.noreply.github.com>
2024-12-26 23:29:59 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,9 @@
+print("This is a Calculator")
+
+a= input("Enter the number: ")
+b= input("Enter the number: ")
+
+print("Addition is",int(a)+int(b))
+print("Subtraction is", int(a)-int(b))
+print("Multiplication is", int(a)*int(b))
+print("Division is", int(a)*int(b))
